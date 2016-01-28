e0e5aad7c15f946fff501f88bb9b5e96e5c17f71
Tomas Weiss <tomasw@salsitasoft.com>
2016-01-28 16:23:48 +0100

    initial commit

---
+++ src/1-counter/main.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
+const ACTION_INCREMENT = 'ACTION_INCREMENT';
+const ACTION_DECREMENT = 'ACTION_DECREMENT';
+
+const update = (model = 0, { type }) => {
+  switch (type) {
+  case ACTION_INCREMENT:
+    return model + 1;
+  case ACTION_DECREMENT:
+    return model - 1;
+  default:
+    return model;
+  }
+};
+
+const countStyle = {
+  fontSize: '20px',
+  fontFamily: 'monospace',
+  display: 'inline-block',
+  width: '50px',
+  textAlign: 'center'
+};
+
+const View = connect(model => ({model}))(({dispatch, model}) => (
+  <div>
+    <button onClick={() => dispatch(ACTION_DECREMENT)}>-</button>
+    <div style={countStyle}>{model}</div>
+    <button onClick={() => dispatch(ACTION_INCREMENT)}>+</button>
+  </div>
+));
+
+const store = createStore(update);
+
+const Application = () => <Provider store={store}><View /></Provider>;
+
+render(<Application />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  debug: true,
+  devtool: 'sourcemap',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/1-counter/main.js'
+  ],
+  output: {
+    path: path.join(__dirname, './dev'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$|\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, './src')
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+};
