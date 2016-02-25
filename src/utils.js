a322f27323e967c54a0e7f62d49a197a2292ff85
Tomas Weiss <tomasw@salsitasoft.com>
2016-02-25 12:59:07 +0100

    tests

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,23 @@
-
+/**
+ * Sums number of characters in array of strings ['abc', 'def'] returns 6
+ *
+ * @param {Array} array of strings
+ * @return {Number} Sum of characters
+ */
 export const sumCharsInArrayOfStrings = arr => arr.reduce((memo, element) => memo + element.length, 0);
 
-export const last = arr => arr[arr.length - 1];
+/**
+ * Returns last element of the provided array
+ *
+ * @param {Array} input array
+ * @return {any} last element of array
+ */
+export const last = arr => arr.length ? arr[arr.length - 1] : null;
 
+/**
+ * Checks whether provided input is type of String
+ *
+ * @param {any} checking input
+ * @return {Bool}
+ */
 export const isString = any => typeof any === 'string';
