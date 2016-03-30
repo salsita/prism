2060e0c8f16c86c6ffa57a3195ab8c2117f79855
Tomas Weiss <tomasw@salsitasoft.com>
2016-03-30 15:14:09 +0200

    mapEffects

---
+++ src/mapEffects.js
@@ -0,0 +1,19 @@
+import forwardTo from './forwardTo';
+import * as Utils from './utils';
+import { sideEffect } from 'redux-side-effects';
+
+// TODO: docs & tests
+export function* mapEffects(iterable, ...types) {
+  const unwoundIterable = Utils.unwindIterable(iterable);
+
+  if (unwoundIterable.length > 1) {
+    for (let i = 0; i < unwoundIterable.length - 2; i++) {
+      const executor = unwoundIterable[i].shift();
+      const mappedExecutor = dispatch => executor(forwardTo(dispatch, ...types));
+
+      yield sideEffect(mappedExecutor, ...unwoundIterable[i]);
+    }
+  }
+
+  return unwoundIterable[unwoundIterable.length - 1];
+}
