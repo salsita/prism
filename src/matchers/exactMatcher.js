4dc35b77e59c946fbed685f3a85085d077066be3
Tomas Weiss <tomasw@salsitasoft.com>
2016-04-07 14:39:04 +0200

    exactMatcher simplifying

--- src/matchers/exactMatcher.js
+++ src/matchers/exactMatcher.js
@@ -1,13 +1,7 @@
-import escapeStringRegexp from 'escape-string-regexp';
-
 export default pattern => {
-  const regexp = new RegExp(`^(${escapeStringRegexp(pattern)})$`);
-
   return action => {
-    const match = action.type.match(regexp);
-
-    if (match) {
-      return [ match[0] ];
+    if (action.type === pattern) {
+      return [ action.type ];
     } else {
       return false;
     }
