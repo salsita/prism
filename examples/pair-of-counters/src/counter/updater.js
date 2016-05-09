1004297aaa066048308f35f1143dd352836befe8
Tomas Weiss <tomasw@salsitasoft.com>
2016-05-09 11:39:15 +0200

    second example ported

--- examples/pair-of-counters/src/counter/updater.js
+++ examples/pair-of-counters/src/counter/updater.js
@@ -1,14 +1,8 @@
-import { Updater, Matchers } from 'redux-elm';
+import { Updater } from 'redux-elm';
 
-export function* init() {
-  return 0;
-}
+export const initialModel = 0;
 
-export default new Updater(init, Matchers.exactMatcher)
-  .case('Increment', function*(model) {
-    return model + 1;
-  })
-  .case('Decrement', function*(model) {
-    return model - 1;
-  })
+export default new Updater(initialModel)
+  .case('Increment', model => model + 1)
+  .case('Decrement', model => model - 1)
   .toReducer();
--- examples/pair-of-counters/src/pair-of-counters/view.js
+++ examples/pair-of-counters/src/pair-of-counters/view.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { forwardTo } from 'redux-elm';
+import { forwardTo, view } from 'redux-elm';
 
 import Counter from '../counter/view';
 
-export default ({ model, dispatch }) => (
+export default view(({ model, dispatch }) => (
   <div>
     <Counter model={model.topCounter} dispatch={forwardTo(dispatch, 'TopCounter')} />
     <Counter model={model.bottomCounter} dispatch={forwardTo(dispatch, 'BottomCounter')} />
     <button onClick={() => dispatch({ type: 'Reset' })}>RESET</button>
   </div>
-);
+));
