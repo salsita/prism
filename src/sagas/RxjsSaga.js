f819e11f3664324f939953db91260fff64a5a91c
Tomas Weiss <tomasw@salsitasoft.com>
2016-08-05 15:13:51 +0200

    unit testing rxjs sagas

--- src/sagas/RxjsSaga.js
+++ src/sagas/RxjsSaga.js
@@ -1,10 +1,11 @@
 import { Subject } from 'rxjs';
 
+const isAction = any => any && typeof any === 'object' && !!any.type;
+
 export default class RxjsSaga {
 
   constructor(saga, model) {
     this.updateModel(model);
-
     this.subject = new Subject();
     this.saga$ = saga(this.subject);
   }
@@ -16,11 +17,12 @@ export default class RxjsSaga {
   subscribe(subscriber) {
     return this
       .saga$
+      .filter(action => isAction(action))
       .subscribe(subscriber);
   }
 
   dispatch(action) {
-    this.subject.next(action);
+    this.subject.next([action, this.model]);
   }
 
   dispose() {
