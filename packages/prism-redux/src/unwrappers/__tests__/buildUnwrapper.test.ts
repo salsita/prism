721b1ecaac0f8f69852f4a6da12643b21a1af88e
Tomas Weiss <tomasw@salsitasoft.com>
2017-02-14 17:29:32 +0100

    unit tests

---
+++ packages/prism-redux/src/unwrappers/__tests__/buildUnwrapper.test.ts
@@ -0,0 +1,33 @@
+import buildUnwrapper from '../buildUnwrapper';
+
+describe('buildUnwrapper', () => {
+  it('should return unwrapped action', () => {
+    const unwrap = buildUnwrapper('Foo');
+
+    expect(unwrap({ type: 'Foo.Bar' })).toEqual({
+      type: 'Bar'
+    });
+  });
+
+  it('should not match when action does not start with pattern', () => {
+    const unwrap = buildUnwrapper('Foo');
+
+    expect(unwrap({ type: 'BarFoo' })).toBeNull();
+    expect(unwrap({ type: 'Bar.Foo' })).toBeNull();
+  });
+
+  it('should return pattern for exact match', () => {
+    const pattern = 'Foo';
+    const unwrap = buildUnwrapper(pattern);
+
+    expect(unwrap({ type: pattern })).toEqual({
+      type: pattern
+    });
+  });
+
+  it('should pass through the content of the action when matched', () => {
+    const unwrap = buildUnwrapper('Foo');
+
+    expect(unwrap({ type: 'Foo.Bar', payload: 42 })).toEqual({ type: 'Bar', payload: 42 });
+  });
+});
